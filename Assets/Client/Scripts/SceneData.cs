4d7cd8f114409f3d21ac40b9940105d1c04d558f
Bibyter <nikitatkarz@gmail.com>
2021-11-10 20:41:57 +0700

    update scene data

--- Assets/Client/Scripts/SceneData.cs
+++ Assets/Client/Scripts/SceneData.cs
@@ -4,22 +4,29 @@ using UnityEngine;
 
 namespace Client
 {
+    // can do separation
     public sealed class SceneData : MonoBehaviour
     {
+        #region
         [SerializeField] Health _playerHealth;
         public Health playerHealth => _playerHealth;
+        #endregion
 
+        #region
         [System.NonSerialized] public float ballDeadBorder;
         [System.NonSerialized] public int fallsCount;
         [System.NonSerialized] public Vector2 ballStartVelocity;
+        #endregion
 
-        public int currentScores;
-        public int bestScores { set { PlayerPrefs.SetInt("bs", value); } get { return PlayerPrefs.GetInt("bs", 0); } }
+        #region
+        [System.NonSerialized] public int currentScores;
+        public int bestScores { private set { PlayerPrefs.SetInt("bs", value); } get { return PlayerPrefs.GetInt("bs", 0); } }
 
         public void BestScoresUpdate(int v)
         {
             bestScores = Mathf.Max(v, bestScores);
             PlayerPrefs.Save();
         }
+        #endregion
     }
 }
